b2df1122fd6f23270141da43fdcf92a692648fab
Topi Kanninen <topi.kanninen@sci.fi>
2022-04-02 13:24:19 +0300

    osa3 cors ja heroku alotus

--- osa3/index.js
+++ osa3/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 let persons =[
 {
@@ -28,6 +29,8 @@ let persons =[
 app.use(express.json())
 
 app.use(morgan('tiny'))
+
+app.use(cors())
 
 app.get('/api/persons', (request, response) =>{
     response.json(persons)
