7bbad4f9c8c78b688bcd5422e46cb7b869611875
Topi Kanninen <topi.kanninen@sci.fi>
2022-04-03 20:06:22 +0300

    osa3 lint

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require('cors')
 const morgan = require('morgan')
 require('dotenv').config()
 const Person = require('./models/person')
-const { nextTick } = require('process')
 
 app.use(express.json())
 
@@ -14,82 +13,79 @@ app.use(morgan('tiny'))
 
 app.use(express.static('build'))
 
-app.get('/api/persons', (request, response) =>{
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
-    response.send(`Phonebook has info of ${persons.length} people</br>${Date()}`)
+  response.send(`Phonebook has info of ${persons.length} people</br>${Date()}`)
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    Person.findById(request.params.id)
-        .then(person => {
-            if (person) {
-                response.json(person)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
-})
-
-app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
-})
-
-app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-    console.log(JSON.stringify(body))
-
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-
-    const person = new Person({
-        id: generateId(),
-        name: body.name,
-        number: body.number,
-    })
-
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
     .catch(error => next(error))
 })
 
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+  console.log(JSON.stringify(body))
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const person = new Person({
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+    .catch(error => next(error))
+})
+
 const generateId = () => {
-    const maxId = Math.floor(Math.random() * 1000)
-    console.log(maxId)
-    return maxId
+  const maxId = Math.floor(Math.random() * 1000)
+  console.log(maxId)
+  return maxId
 }
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message)
-    
-    if (error.name === 'CastError'){
-        return response.status(400).send({ error: 'malformatted id'})
-    } else if (error.name === 'ValidationError') {
+  console.log(error.message)
+  if (error.name === 'CastError'){
+    return response.status(400).send({ error: 'malformatted id'})
+  } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
-    }
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
@@ -97,5 +93,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
