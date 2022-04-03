8f50c8a80711bb7fb0f46fd42c479249d5ec02b3
Topi Kanninen <topi.kanninen@sci.fi>
2022-04-03 20:12:58 +0300

    osa3 eslint

--- models/person.js
+++ models/person.js
@@ -4,28 +4,28 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
-    const personSchema = new mongoose.Schema({
-        id: Number,
-        name: {
-            type: String,
-            minlength: 3
-        },
-        number: String,
-    })
+const personSchema = new mongoose.Schema({
+  id: Number,
+  name: {
+    type: String,
+    minlength: 3
+  },
+  number: String,
+})
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject._v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject._v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
